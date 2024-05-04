0638b4f2020a5645e6d2a3eb9ebd377ba7d785d8
sorada1111 <sorada1111@gmail.com>
2024-05-04 16:55:07 -0400

    Add checkout form validation, Closes #20

---
+++ src/app/validators/sp-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { SpShopValidators } from './sp-shop-validators';
+
+describe('SpShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new SpShopValidators()).toBeTruthy();
+  });
+});
