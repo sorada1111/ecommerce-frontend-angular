3d60658b03dc64391dfd369d35676551e46ba758
sorada1111 <sorada1111@gmail.com>
2024-05-08 14:16:38 -0400

    fix refresh that lost all the products in the cart after refresh, Closes #30

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,17 +1,17 @@
-export default {
-  oidc: {
-    clientId: '0oagyazb58d3MYUTL5d7',
-    issuer: 'https://dev-28378352.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
-    scopes: ['openid', 'profile', 'email'],
-  },
-};
-
 // export default {
 //   oidc: {
-//     clientId: '0oagx9n8sv16M2SpL5d7',
+//     clientId: '0oagyazb58d3MYUTL5d7',
 //     issuer: 'https://dev-28378352.okta.com/oauth2/default',
-//     redirectUri: 'https://ecommerce-spring-angular.netlify.app/login/callback',
+//     redirectUri: 'http://localhost:4200/login/callback',
 //     scopes: ['openid', 'profile', 'email'],
 //   },
 // };
+
+export default {
+  oidc: {
+    clientId: '0oagx9n8sv16M2SpL5d7',
+    issuer: 'https://dev-28378352.okta.com/oauth2/default',
+    redirectUri: 'https://ecommerce-spring-angular.netlify.app/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
